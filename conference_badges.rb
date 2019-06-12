76f3a9bda1e23f3c7645da5cd04bc9dc7a8ce909
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 19:21:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(attendees)
    array = []
    #each_with_index: keep track of the index number of the current iteration
    attendees.each_with_index do |name, index|
-      array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+      array << "Hello, #{name}! You'll be assigned to room #{index+1}!"
    end
    array
 end
