114e0b7d2735c5726a60b694525987761c5b57aa
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 19:21:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,10 +6,14 @@ def badge_maker(name)
    return "Hello, my name is #{name}."
 end
 
-# def batch_badge_creator
-#
-# end
-#
+def batch_badge_creator(attendees)
+   #create new array to put name inside
+   array = []
+   #loop through each name provided and push the message to new array with the name
+   attendees.each { |name| array << badge_maker(name) }
+   array #return the new array
+end
+
 # def assign_rooms
 #
 # end
