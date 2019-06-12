80111296c4725f1dea800ddc3c9e834db9b7b1cd
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 19:21:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,10 +14,17 @@ def batch_badge_creator(attendees)
    array #return the new array
 end
 
-# def assign_rooms
-#
-# end
-#
-# def printer
-#
-# end
+def assign_rooms(attendees)
+   array = []
+   #each_with_index: keep track of the index number of the current iteration
+   attendees.each_with_index do |name, index|
+      array << "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+   end
+   array
+end
+
+def printer(attendees)
+#print the name of each index in the array
+   batch_badge_creator(attendees).each { |names| puts names }
+   assign_rooms(attendees).each { |room| puts room }
+end
