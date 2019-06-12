4f85f041fcf808ea96bbce4f7b7cd82015682f15
Phyllis Kan <pyk9q7@gmail.com>
2019-06-12 19:17:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ puts "Hello, _____! You'll be assigned to room _____!"
 end
 
 def badge_maker(Arel)
-  puts "Hello, my name is Arel."
+  puts "Hello, my name is #{name}."
 end
 
 # def batch_badge_creator
